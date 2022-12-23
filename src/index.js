7efb14e49d0ed2b2ff4f75979c35d5748b86a791
Kate <111576086+ShpakKate@users.noreply.github.com>
2022-12-23 13:14:08 +0300

    change logik

--- src/index.js
+++ src/index.js
@@ -1,42 +1,73 @@
-import('./scss/main.scss')
+import('./scss/main.scss');
+
+function regimeCalcChange() {
+    if (calculator.classList.contains('calc-small-screen')) {
+        calculator.classList.remove('calc-small-screen');
+        calculator.classList.add('calc-full-screen');
+        screen.classList.add('calc-screen-full-size');
+    } else {
+        calculator.classList.remove('calc-full-screen');
+        calculator.classList.add('calc-small-screen');
+        screen.classList.remove('calc-screen-full-size');
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const screen = document.querySelector('#screen');
-    const buttonsField = document.querySelector('#buttons');
+    const calculator = document.querySelector('#calculator');
     const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
     const action = ['+', '-', '÷', 'X'];
 
     let a = '';
     let b = '';
     let sign = '';
-    screen.textContent = '';
     let finish = false;
     let arrNums = ['0'];
+    let arrNums2 = ['0'];
     let arrStr = [];
     let result = '';
 
-    console.log(arrNums[arrNums.length - 1] === '0')
+    screen.textContent = '';
 
     function clearScreen() {
         a = '';
         b = '';
         sign = '';
+        result = '';
         arrNums = ['0'];
+        arrNums2 = ['0'];
         arrStr = [];
 
-        screen.classList.remove('screen-font-size');
+        screen.classList.remove('screen-size');
+        screen.style.color = '';
         screen.textContent = 0;
     }
 
     function operandSelection(operand) {
         sign = operand;
         screen.textContent = sign;
+        arrNums2 = ['0'];
         console.log(a, sign, b);
     }
 
+    function checkFullScreen() {
+        if (screen.classList.contains('screen-size')) {
+            screen.classList.remove('screen-size');
+        }
+
+        if (finish) {
+            let arrNums = ['0'];
+            let arrNums2 = ['0'];
+            screen.style.color = '';
+        }
+    }
+
     function numbersSelection(num) {
-        if (b === '' && sign === '') {
+        checkFullScreen();
+
+        if (b === '' && sign === '' && arrNums.length < 11) {
+
             if (arrNums.length === 1) {
                 screen.textContent = '0';
                 arrNums.push(num);
@@ -46,26 +77,40 @@ document.addEventListener("DOMContentLoaded", () => {
                 screen.textContent = a;
                 arrNums.push(num);
                 console.log(a, sign, b);
-            } else {
-                if (arrNums[arrNums.length - 1] === '0') return;
             }
+            else if (arrNums[arrNums.length - 1] === '0') return;
+
         }
-        else if (b !== '' && sign !== '' && finish) {
-            a = result;
-            b = num;
-            finish = false;
-            screen.textContent = b;
+        else if (b !== '' && sign !== '' && finish && arrNums2.length < 9) {
+
+            if (num === '0' && arrNums2.length > 2 || num !== '0') {
+                a = result;
+                b = num;
+                finish = false;
+                screen.textContent = b;
+                arrNums2.push(num);
+                console.log(a, sign, b);
+            }
+            else if (arrNums2[arrNums2.length - 1] === '0') return;
+
         }
-        else {
+        else if ((num === '0' && arrNums2.length >= 2 || num === '0' && arrNums2.length > 2 || num !== '0') && sign !== '' && arrNums2.length < 10) {
             b += num;
             screen.textContent = b;
-            console.log(a, sign, b)
+            arrNums2.push(num);
+            console.log(a, sign, b);
         }
+        else if (arrNums2[arrNums2.length - 1] === '0') {
+            screen.textContent = '0';
+            return;
+        }
+
         return finish;
     }
 
     function resultCalculation(a, b, sign) {
-        if (b === '') b = a;
+        if (b === '' && sign !== '÷') b = a;
+
         switch (sign) {
             case ('+'):
                 a = +a + (+b);
@@ -74,18 +119,19 @@ document.addEventListener("DOMContentLoaded", () => {
                 a = a - b;
                 break;
             case ('÷'):
-                if (b === '0') {
+                if (b === '') {
                     screen.style.color = 'red';
+                    screen.classList.add('screen-size');
                     screen.textContent = 'на 0 делить нельзя';
-                }
-                a = a / b;
+                    return;
+                } else a = a / b;
                 break;
             case ('X'):
                 a = a * b;
         }
         result = a;
         finish = true;
-        screen.textContent = a;
+        screen.textContent = result;
         console.log(a, sign, b)
         return finish;
     }
@@ -99,7 +145,6 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log(a, sign, b)
             screen.textContent = a;
         }
-
         return finish;
     }
 
@@ -116,25 +161,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    function numberCheck(num) {
-        Math.round(num * 100 / 100);
-
-        arrStr = num.toString().split('');
+    function resultCheck(result) {
+        arrStr = result.toString().split('');
 
         if (arrStr.length > 9) {
-            screen.classList.add('screen-font-size');
+            screen.classList.add('screen-size');
         }
     }
 
-    buttonsField.addEventListener('click', event => {
-
-        console.log(arrNums);
+    document.querySelector('#buttons').addEventListener('click', event => {
 
         const key = event.target.textContent;
-
-        if (event.target == document.querySelector('#ac')) {
-            clearScreen();
-        }
 
         if (event.target === document.querySelector('#equal')) {
             resultCalculation(a, b, sign)
@@ -149,6 +186,12 @@ document.addEventListener("DOMContentLoaded", () => {
             arrNums = ['0'];
         }
 
+        resultCheck(result);
+
+        if (event.target == document.querySelector('#ac')) {
+            clearScreen();
+        }
+
         if (digit.includes(key)) {
             numbersSelection(key)
         }
@@ -156,9 +199,16 @@ document.addEventListener("DOMContentLoaded", () => {
         if (action.includes(key)) {
             operandSelection(key)
         }
-
-        numberCheck(result);
-        numberCheck(a);
-        numberCheck(b);
     });
+
+    document.querySelector('#calc-control').addEventListener('click', event => {
+
+        if (event.target === document.querySelector('#close')) {
+            calculator.classList.add('hidden');
+        }
+
+        if (event.target === document.querySelector('#full-screen-btn')) {
+            regimeCalcChange();
+        }
+    })
 });
